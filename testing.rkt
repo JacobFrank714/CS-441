ebd8297efc8ac0967975116f35c594b7f4d86adf
mcfreak12 <jacobfrank714@gmail.com>
2022-09-23 11:39:25 -0500

    setting up outline

--- testing.rkt
+++ testing.rkt
@@ -1,8 +1,99 @@
 #lang racket
 
-(define (greeting name)
-(string-append name" Hello"))
+(define (mult_op? x)
+    (cond
+        [(= x "*") #t]
+        [(= x "/") #t]
+        [else #f]
+    )
+)
 
-(greeting "jacob")
+(define (add_op? x)
+    (cond   
+        [(= x "+") #t]
+        [(= x "-") #t]
+        [else #f]
+    )
+)
 
+(define (factor_tail? x)
+    (cond
+        [(mult_op? (x))  #t]
+        [(factor? (x)) #t]
+        [(factor_tail? (x)) #t]
+        [("") #t]
+        [else #f]
+    )
+)
 
+(define (factor? x)
+    (cond
+        [(= x "(") #t]
+        [(expr? (x)) #t]
+        [(= x ")") #t]
+        [else #f]
+    )
+)
+
+(define (term_tail? x)
+    (cond
+        [((add_op? (x))) #t]
+        [(term? (x)) #t]
+        [(term_tail? (x)) #t]
+        [(= x "") #t]
+        [else #f]
+    )
+)
+
+(define (term? x)
+    (cond 
+        [(factor? (x)) #t]
+        [(factor_tail? (x)) #t]
+        [else #f]
+    )
+)
+
+(define (expr? x)
+    (cond
+        [(term? (x)) #t]
+        [(term_tail? (x)) #t]
+        [else #f]))
+
+(define (stmt? x)
+    (cond 
+        [(= x "id") #t]
+        [(= x "read") #t]
+        [(= x "write") (expr?(x))]
+        [else #f]
+    )
+)
+
+(define (stmt_list? x)
+    (cond
+        [(stmt?(x)) #t]
+        [(stmt_list?(x)) #t]
+        [(= x "") #t]
+        [else #f]
+    )
+)
+
+(define (program? x) 
+    (if (stmt_list?(x))
+        (if(= x "$$") 
+            #t 
+            #f
+        ) 
+        #f
+    )
+)
+
+(define (parse str)
+    str
+    (define lines (open-input-file str))
+    lines
+    (define prog? (program? read-line(lines)))
+    prog?
+)
+
+(define isProgram?(parse "test.txt"))
+isProgram?

06bb9c0a40e23a8936ab42928511b2973241adca
mcfreak12 <jacobfrank714@gmail.com>
2022-09-28 13:01:15 -0500

    can read in the tokens and started parsing

--- parser.rkt
+++ parser.rkt
@@ -4,85 +4,92 @@
 (require parser-tools/lex)
 
 (define (mult_op? x)
+    (println "checking mult_op?")
     (cond
-        [(= x "*") #t]
-        [(= x "/") #t]
+        [(eq? (token-name x) 'MULT-OP) #t]
         [else #f]
     )
 )
 
 (define (add_op? x)
+    (println "checking add_op?")
     (cond   
-        [(= x "+") #t]
-        [(= x "-") #t]
+        [(eq? (token-name x) 'ADD-OP) #t]
         [else #f]
     )
 )
 
 (define (factor_tail? x)
+    (println "checking factor_tail?")
     (cond
-        [(mult_op? (x))  #t]
-        [(factor? (x)) #t]
-        [(factor_tail? (x)) #t]
-        [("") #t]
+        [(mult_op? (first x))  #t]
+        [(factor? (first x)) #t]
+        [(factor_tail? (first x)) #t]
+        [(eq? (token-name (first x)) 'newline) #t]
         [else #f]
     )
 )
 
 (define (factor? x)
+    (println "checking factor?")
     (cond
-        [(= x "(") #t]
-        [(expr? (x)) #t]
-        [(= x ")") #t]
+        [(and (eq? (token-name (first x)) 'PAREN-START) (expr? (rest x)) (eq? (token-name (third x)) 'PAREN-END)) #t]
+        [(eq? (token-name (first x)) 'IDENTIFIER) #t]
+        [(eq? (token-name (first x)) 'NUMBER) #t]
         [else #f]
     )
 )
 
 (define (term_tail? x)
+    (println "checking term_tail?")
     (cond
-        [((add_op? (x))) #t]
-        [(term? (x)) #t]
-        [(term_tail? (x)) #t]
-        [(= x "") #t]
+        [(add_op? (first x)) #t]
+        [(term? (first x)) #t]
+        [(term_tail? (first x)) #t]
+        [(eq? (token-name (first x)) 'newline) #t]
         [else #f]
     )
 )
 
 (define (term? x)
+    (println "checking term?")
     (cond 
-        [(factor? (x)) #t]
-        [(factor_tail? (x)) #t]
+        [(factor? (first x)) #t]
+        [(factor_tail? (first x)) #t]
         [else #f]
     )
 )
 
 (define (expr? x)
+    (println "checking expr?")
     (cond
-        [(term? (x)) #t]
-        [(term_tail? (x)) #t]
+        [(term? (first x)) #t]
+        [(term_tail? (first x)) #t]
         [else #f]))
 
 (define (stmt? x)
+    (println "checking stmt?")
     (cond 
-        [(= x "id") #t]
-        [(= x "read") #t]
-        [(= x "write") (expr?(x))]
+        [(eq? (token-name x) 'IDENTIFIER) #t]
+        [(eq? (token-name x) 'READ) #t]
+        [(eq? (token-name x) 'WRITE) (expr? (rest x))]
         [else #f]
     )
 )
 
 (define (stmt_list? x)
+    (println "checking stmt_list?")
     (cond
-        [(stmt?(x)) #t]
-        [(stmt_list?(x)) #t]
-        [(= x "") #t]
+        [(stmt? (first x)) (stmt_list? (rest x))]
+        [(eq? (token-name (first x)) 'newline) #t]
         [else #f]
     )
 )
 
 (define (program? x) 
-    (if (stmt_list?(x))
-        (if(= x "$$") 
+    (println "checking program?")
+    (if (stmt_list? x)
+        (if(eq? (token-name last x) 'END-OF-PROGRAM) 
             #t 
             #f
         ) 
@@ -95,7 +102,9 @@
     prog?
 )
 
-(define tokens(map position-token-token (string->tokens "input02.txt")))
-tokens
-;;; (define isProgram?(parse tokens))
-;;; isProgram?
+(define tokens(map position-token-token (string->tokens "input01.txt")))
+;;; tokens
+(define isProgram?(parse tokens))
+isProgram?
+
+;;; gramar on page 107 of book
